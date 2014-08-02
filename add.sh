a51e007ef58577ce525affb53630016b88bab649
Megumi Hatakeyama <megumi.htk@gmail.com>
2014-08-02 19:47:41 +0900

    initial commit

---
+++ add.sh
@@ -0,0 +1,7 @@
+
+git submodule add git://github.com/fuel/core.git fuel/core
+git submodule add git://github.com/fuel/oil.git fuel/packages/oil
+git submodule add git://github.com/fuel/auth.git fuel/packages/auth
+git submodule add git://github.com/fuel/parser.git fuel/packages/parser
+git submodule add git://github.com/fuel/orm.git fuel/packages/orm
+git submodule add git://github.com/fuel/email.git fuel/packages/email
---
+++ fuel/app/logs/2014/08/02.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2014-08-02 10:34:38 --> Error - date_default_timezone_get(): It is not safe to rely on the system's timezone settings. You are *required* to use the date.timezone setting or the date_default_timezone_set() function. In case you used any of those methods and you are still getting this warning, you most likely misspelled the timezone identifier. We selected the timezone 'UTC' for now, but please set date.timezone to select your timezone. in /Users/megumi/Documents/Dropbox/mas-sys/fuelphp/branchtest/fuel/core/classes/fuel.php on line 167
